c833ff82c3a2304a9b9c9b0c357f8760d789cb81
zhao-xtao <96565047+zhao-xtao@users.noreply.github.com>
2023-08-09 11:10:53 +0800

    Create 15.cpp

---
+++ 15.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int> > res;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        if (nums[0] > 0 || nums[n - 1] < 0) return res;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1, k = n - 1;
+            while(j < k) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+                    k--;
+                } else if (nums[i] + nums[j] + nums[k] > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return res;
+    }
+};
